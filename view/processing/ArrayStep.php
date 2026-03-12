4aeebb2f10429a3e55e7040aedab7d1f703ba5c6
Simone Cieri <cieri78@gmail.com>
2026-03-12 15:03:12 +0000

    array step

--- view/processing/ArrayStep.php
+++ view/processing/ArrayStep.php
@@ -10,22 +10,18 @@
         <?php
             if (is_array($ArrayStep) && count($ArrayStep) > 0) {
                 foreach ($ArrayStep as $row) {
-                    $pos = $row['POS'];
-                    $idRunSh = $row['ID_RUN_SH'];
-                    $step = $row['STEP'];
-                    $time = $row['TIME'];
+                    $step = isset($row['STEP']) ? $row['STEP'] : '';
+                    $time = isset($row['TIME']) ? $row['TIME'] : '';
                     ?>
                     <tr class="array-step-row">
-                        <th>Pos</th><td><?php echo $pos; ?></td>
-                        <th>ID Run SH</th><td><?php echo $idRunSh; ?></td>
-                        <th>Step</th><td><?php echo $step; ?></td>
-                        <th>Data/Ora</th><td><?php echo $time; ?></td>
-                        <th>Azioni</th><td><button class="btn-small" onclick="alert('Step: <?php echo htmlspecialchars($step); ?>')">Info</button></td>
+                        <th class="array-step-marker"></th>
+                        <th>Eseguo Shell in WORK :</th><td class="array-step-desc"><?php echo htmlspecialchars($step, ENT_QUOTES, 'UTF-8'); ?></td>
+                        <th>TimeStamp</th><td class="array-step-time"><?php echo htmlspecialchars($time, ENT_QUOTES, 'UTF-8'); ?></td>
                     </tr>
                     <?php
                 }
             } else {
-                echo "<tr><td colspan='10'>Nessuno step disponibile</td></tr>";
+                echo "<tr><td colspan='5'>Nessuno step disponibile</td></tr>";
             }
         ?>
     </tbody>
